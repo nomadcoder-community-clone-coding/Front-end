42aa510c374ef9799a17050585d390a324a15a69
LEEYUNJI <yunji965@gmail.com>
2021-05-30 16:33:47 +0900

    Title 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import styled from 'styled-components';
 import Header from './component/Header';
 
+const Title = styled.div`
+  padding-top: 65px;
+  font-size: 50px;
+  text-align: center;
+`
+const SubTitle = styled.div`
+  margin-top: .75rem;
+  font-size: 22px;
+  text-align: center;
+`
+
 function App() {
   return (
-    <Header />
+    <div>
+      <Header />
+      <Title>Community</Title>
+      <SubTitle>개발자 99% 커뮤니티에서 수다 떨어요!</SubTitle>
+    </div>
   );
 }
 
