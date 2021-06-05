4a46a66a845d5a30852795ed351c410d86bd1362
nitronium102 <nitronium@ewhain.net>
2021-06-05 10:40:20 +0900

    Mainboard css 수정

--- src/component/MainBoard.js
+++ src/component/MainBoard.js
@@ -1,5 +1,51 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+<<<<<<< Updated upstream
+=======
+import styled from 'styled-components';
+
+const Container = styled.div`
+    background-color: #F8F8F8;
+    border-radius: 7px;
+    box-shadow: 0px 6px 4px rgba(0, 0, 0, 0.2);
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width:707px;
+    height:115px;
+    margin-bottom: 15px;
+`
+
+const Contents = styled.div`
+    font : semibold;
+    display: flex;
+    flex-direction: row;
+`
+
+const LikeBox = styled.div`
+  background: transparent !important;
+  border: 1.5px solid #9B9F9D;
+  border-radius: 5px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-left: 25px;
+  margin-top: 28px;
+  width:44px;
+  height:44px;
+`
+
+const CategoryButton = styled.button`
+  font : semibold;
+  background: #6B7280;
+  border-radius: .125rem;
+  border: none;
+  outline: none;
+  color: white;
+`
+
+>>>>>>> Stashed changes
 
 // useState : 
 // useEffect : 화면에 렌더링이 완료된 이후에 수행
@@ -41,9 +87,32 @@ function MainBoard() {
 
     return (
         <div>
+<<<<<<< Updated upstream
             우왕
         </div>
     )
+=======
+            <div>
+                {name.map(content => (
+                    <Container
+                        key={content.id}>
+                        <Contents>
+                            <LikeBox>{content.likes}</LikeBox>
+                            <div>
+                                <h3 style={{ marginLeft: "30px" }}>
+                                    {content.title}</h3>
+                                <p style={{ marginLeft: "30px", fontSize: "16px" }}>in&nbsp;
+                    <CategoryButton>#{content.category}</CategoryButton> by {content.writer}
+                    &emsp; * {content.createdDate} &nbsp;* {content.commentNum}
+                                </p>
+                            </div>
+                        </Contents>
+                    </Container>
+                ))}
+            </div>
+        </div >
+    );
+>>>>>>> Stashed changes
 }
 
 export default MainBoard;
